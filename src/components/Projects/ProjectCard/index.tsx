56691aeb9a07aa750ddf1cf149e0baee7e696d4d
Robert Kozik <rkozik98@gmail.com>
2021-02-22 23:18:30 +0100

    clean unuser vars and imports

--- src/components/Projects/ProjectCard/index.tsx
+++ src/components/Projects/ProjectCard/index.tsx
@@ -1,6 +1,5 @@
 import './style.css';
 import PhotoDisplay from '../../PhotoDisplay';
-import { RefObject } from 'react';
 
 interface Props {
     imgs: Array<string>,
@@ -8,10 +7,9 @@ interface Props {
     description: string,
     name: string,
     git?: string,
-    live?: string,
 }
 
-const ProjectCard = ({imgs, techs, name, description}: Props) => {
+const ProjectCard = ({imgs, techs, name, description, git}: Props) => {
 
 
     return (
@@ -22,14 +20,10 @@ const ProjectCard = ({imgs, techs, name, description}: Props) => {
             <div className='description-card-container'>
                 <div className="card-desc">
                     <div className="card-header">
-                        <h1>{name}</h1>
+                        <a href={git}><h1>{name}</h1></a>
                         <div className="tech-outer-container">
                             <PhotoDisplay images={techs} round={true} transitionTime={3000} />
                         </div>
-                        {/* <div className="link-container">
-                            <button>Repo</button>
-                            <button>Live</button>
-                        </div> */}
                     </div>
                 </div>
                 <p>{description}</p>
